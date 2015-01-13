2cb398d849de459f6af48fc4902dcd94526de966
benoitbrb <benoitbarbe@live.fr>
2015-01-13 15:57:14 +0100

    recherche

--- src/main/java/fr/mediashare/ressources/RechercheResource.java
+++ src/main/java/fr/mediashare/ressources/RechercheResource.java
@@ -1,4 +1,4 @@
-package fr.iutinfo;
+package fr.mediashare.ressources;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -7,22 +7,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import fr.mediashare.model.FeedBack;
-import fr.mediashare.model.User;
 
-
-@Path("/signin")
+@Path("/recherche")
 @Produces(MediaType.APPLICATION_JSON)
-public class InscriptionResource {
-
+public class RechercheResource {
+	
 	@GET 
 	public FeedBack getTest() {
+		//FeedBack fb = 
+		//fb.setMessage("ok");
+		//fb.setSuccess(0);
 		return new FeedBack(true, "ok");
 	}
 	
 	@POST
-	public FeedBack signIn(User user) {
-		// select bdd
-		if(user.getPseudo().equals("toto")) {
+	public FeedBack signIn(String recherche) {
+		if(recherche.equals("toto")) {
 			return new FeedBack(true, "Inscription réussie");
 		} else {
 			return new FeedBack(false, "pseudo différent de toto");
