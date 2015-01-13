fbda296c1f04d29ff3094fbf0a2e6ba27324fa79
Maximilien DEROO <maximilien.deroo@etudiant.univ-lille1.fr>
2015-01-13 15:07:28 +0100

    Connexion

--- src/main/java/fr/iutinfo/InscriptionResource.java
+++ src/main/java/fr/iutinfo/InscriptionResource.java
@@ -6,6 +6,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import fr.mediashare.model.FeedBack;
+import fr.mediashare.model.User;
+
 
 @Path("/signin")
 @Produces(MediaType.APPLICATION_JSON)
--- src/main/webapp/js/connexion.js
+++ src/main/webapp/js/connexion.js
@@ -1,4 +1,19 @@
 function connect(login, password){
-	
+	$.getJSON("v1/connexion/" + login, function(data) {
 		
+		console.log(data)
+		if(data.success)
+			$("#reponse").html('login ok');
+		else
+			$("#reponse").html('login pas ok');
+	});
+	
+	$.getJSON("v1/connexion/" + password, function(data) {
+		
+		console.log(data)
+		if(data.success)
+			$("#reponse").html('password ok');
+		else
+			$("#reponse").html('password pas ok');
+	});
 }
