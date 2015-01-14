e04acd2d8e1d7485396da20f501a274359c8800d
Louis Guilbert <louis.guilbert@etudiant.univ-lille1.fr>
2015-01-14 17:27:28 +0100

    modif path upload (test 10)

--- src/main/java/fr/mediashare/servlet/UploadServlet.java
+++ src/main/java/fr/mediashare/servlet/UploadServlet.java
@@ -51,7 +51,7 @@ public class UploadServlet extends HttpServlet {
          
         for (Part part : request.getParts()) {
             String fileName = extractFileName(part);
-            part.write(savePath + File.separator + "titi.txt");
+            part.write(savePath + File.separator + fileName);
         }
  
         request.setAttribute("message", "Upload has been done successfully!");
