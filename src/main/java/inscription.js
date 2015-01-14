7c140cd3bbb16aa605ea835ffbbc58cfc2b0391a
PP-J <p.p-j@hotmail.fr>
2015-01-14 11:44:54 +0100

    fix sql

---
+++ src/main/java/inscription.js
@@ -0,0 +1,28 @@
+function register(pseudo, email, mdp, mdp2){
+	if (mdp == mdp2){
+		$.ajax({
+			type : 'POST',
+			contentType : 'application/json',
+			url : "v1/register/",
+			dataType : "json",
+			data : JSON.stringify({
+				"email" : email,
+				"pseudo": pseudo,
+				"mdp" : mdp,
+				"mdp2" : mdp2
+			}),
+			success : function(data, textStatus, jqXHR) {
+				console.log(data)
+				if(data.success)
+					window.location.replace("murGeneral.html");
+				else
+					$("#reponse").html(data.message);
+			},
+			error : function(jqXHR, textStatus, errorThrown) {
+				alert('erreur');
+			}
+		});
+	}
+	else
+		$("#reponse").html('Erreur, mots de passe différents');
+}
