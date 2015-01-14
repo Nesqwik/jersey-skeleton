31deb540c036794102bf275d60329b165c598935
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2015-01-14 16:22:44 +0100

    suppresion jar

--- src/main/java/fr/mediashare/App.java
+++ src/main/java/fr/mediashare/App.java
@@ -13,15 +13,12 @@ import fr.mediashare.ressources.InscriptionResource;
 import fr.mediashare.ressources.MurGeneral;
 import fr.mediashare.ressources.RechercheResource;
 import fr.mediashare.ressources.SupressionAdminResource;
-import fr.mediashare.ressources.UploadResource;
-
 
 @ApplicationPath("/v1/")
 public class App extends Application{
     @Override
     public Set<Class<?>> getClasses() {
     	Set<Class<?>> s = new HashSet<Class<?>>();
-    	s.add(UploadResource.class);
     	s.add(MultiPartFeature.class);
     	s.add(InscriptionResource.class);
     	s.add(ConnexionResource.class);
