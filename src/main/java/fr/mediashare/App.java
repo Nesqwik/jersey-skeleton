f4646f37d79e2dd9fa5664f8d16b91c5735e5a8a
Maximilien DEROO <maximilien.deroo@etudiant.univ-lille1.fr>
2015-01-15 10:56:09 +0100

    Modification du profil

--- src/main/java/fr/mediashare/App.java
+++ src/main/java/fr/mediashare/App.java
@@ -11,6 +11,7 @@ import org.glassfish.jersey.filter.LoggingFilter;
 
 import fr.mediashare.ressources.ConnexionResource;
 import fr.mediashare.ressources.InscriptionResource;
+import fr.mediashare.ressources.ModifierProfilResource;
 import fr.mediashare.ressources.MurGeneral;
 import fr.mediashare.ressources.RechercheResource;
 import fr.mediashare.ressources.SupressionAdminResource;
@@ -27,6 +28,7 @@ public class App extends Application{
     	s.add(RechercheResource.class);
     	s.add(ConnexionResource.class);
     	s.add(SupressionAdminResource.class);
+    	s.add(ModifierProfilResource.class);
     	return s;
     }
 }
