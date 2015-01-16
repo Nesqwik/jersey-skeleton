14fff1b16fee57d5627f75e10d34761c752443e1
PP-J <p.p-j@hotmail.fr>
2015-01-16 11:42:07 +0100

    ajout lecture pdf

--- src/main/webapp/js/murGeneral.js
+++ src/main/webapp/js/murGeneral.js
@@ -15,6 +15,10 @@ function reload(){
 				content += "<div class=\"pseudo\">" + data[i].pseudo + "</div>";
 				content += "<div class=\"description\">" + data[i].description + "</div>";
 				content += "<div class=\"download\"><a href=\""+data[i].path+"\" download=\""+data[i].path+"\">Télécharger</a></div>"; 
+				content += "<div class=\"post\">";
+				content += "<div class=\"pseudo\">" + data[i].pseudo + "</div>";
+				content += "<div class=\"description\">" + data[i].description + "</div>";
+				content += "<div class=\"download\"><a href=\""+data[i].path+"\" download=\""+data[i].path+"\">Télécharger</a></div>"; 
 				if(data[i].type == "audio")
 					content += "<audio controls=\"controls\"> <source src=\"" + data[i].path + "\" type=\"audio/mp3\" /> Votre navigateur n'est pas compatible </audio>";
 				
@@ -22,8 +26,11 @@ function reload(){
 					content += "<video width=\"320\" height=\"240\" controls src=\"" + data[i].path + "\"> video mp4 </video>";
 				
 				if(data[i].type == "image")
-					content += "<image width=\"320\" height=\"240\" src=\" " + data[i].path + "\" class=\"img-responsive\"></image>";
-					content += "</div>";
+					content += "<image controls src=\" " + data[i].path + "\"></image>";
+				if(data[i].path.split(".")[1] == "pdf"){
+					content += "<iframe src=\""+data[i].path+"\" width=\"500\" height=\"500\" align=\"middle\"></iframe>"
+				}
+				content += "</div>";
 			}
 			$("#posts").html(content);
 		},
