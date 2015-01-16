6feba0a6fd38752539e42da42e168314f3cc3197
Louis Guilbert <louis.guilbert@etudiant.univ-lille1.fr>
2015-01-16 12:42:08 +0100

    modif css + recherche html

--- src/main/webapp/js/murGeneral.js
+++ src/main/webapp/js/murGeneral.js
@@ -19,10 +19,10 @@ function reload(){
 					content += "<audio controls=\"controls\"> <source src=\"" + data[i].path + "\" type=\"audio/mp3\" /> Votre navigateur n'est pas compatible </audio>";
 				
 				if(data[i].type == "video")
-					content += "<video width=\"320\" height=\"240\" controls src=\"" + data[i].path + "\"> video mp4 </video>";
+					content += "<video width=\"500\" height=\"300\" controls src=\"" + data[i].path + "\"> video mp4 </video>";
 				
 				if(data[i].type == "image")
-					content += "<image width=\"320\" height=\"240\" controls src=\" " + data[i].path + "\"></image>";
+					content += "<image width=\"500\" controls src=\" " + data[i].path + "\"></image>";
 				if(data[i].path.split(".")[1] == "pdf"){
 					content += "<iframe src=\""+data[i].path+"\" width=\"500\" height=\"500\" align=\"middle\"></iframe>"
 				}
